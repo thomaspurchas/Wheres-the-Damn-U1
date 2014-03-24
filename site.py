e053934e4dd5126693eeaec576989994416f08cb
Thomas Purchas <tpurchas@gmail.com>
2014-03-24 02:59:59 +0000

    Initial Commit

---
+++ site.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+from bottle import route, request, run, template, install, static_file
+from bottle.ext import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, Sequence, String
+from sqlalchemy.ext.declarative import declarative_base
+from geoalchemy2 import Geometry
+
+import json
+import os
+
+# Guess if we are running on Heroku at the moment
+HEROKU = True if os.environ.get("DATABASE_URL", None) else False
+DEBUG = HEROKU
+DATABASE_URL = os.environ.get("DATABASE_URL", 'postgresql://kjntea_omsysv:35deb151@spacialdb.com:9999/kjntea_omsysv')
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL, echo=DEBUG)
+
+plugin = sqlalchemy.Plugin(
+    engine, # SQLAlchemy engine created with create_engine function.
+    Base.metadata, # SQLAlchemy metadata, required only if create=True.
+    keyword='db', # Keyword used to inject session database in a route (default 'db').
+    create=True, # If it is true, execute `metadata.create_all(engine)` when plugin is applied (default False).
+)
+
+install(plugin)
+
+class BusStop(Base):
+    __tablename__ = 'BusStops'
+    id = Column(Integer, Sequence('id_seq'), primary_key=True)
+    name = Column(String(50))
+    location = Column(Geometry('POINT'))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<BusStop('%s', '%s', '%s')>" % (self.id, self.name, self.location)
+
+    def __str__(self):
+        return "BusStop('%s', '%s', '%s')" % (self.id, self.name, self.location)
+
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    gets = dict(request.query.decode())
+
+    return template('Hello {{name}}, how are you? {{gets}}', name=name, gets=gets)
+
+@route('/addstop')
+def addstop(db):
+    query = request.query.decode()
+    name = query['name']
+    lat = float(query['lat'])
+    lon = float(query['lon'])
+
+    stop = BusStop(name)
+    stop.location = 'POINT(%s %s)' % (lon, lat)
+
+    db.add(stop)
+
+    return "Stop: " + str(stop)
+
+@route('/stops')
+def getstops(db):
+
+    request_data = request.query.decode()
+    if 'lon' in request_data and 'lat' in request_data:
+        location = (request.query.decode()['lon'], request.query.decode()['lat'])
+        location = 'POINT(%s %s)' % location
+    else:
+        location = None
+
+    query = db.query(BusStop.id,
+                     BusStop.name,
+                     BusStop.location.ST_AsGeoJSON().label('geo')
+                     )
+
+    if location:
+        query = query.add_columns(
+                BusStop.location.ST_Distance_Sphere(location).label('distance')
+                ).order_by('distance')
+
+    stops = []
+
+    for stop in query:
+        print stop
+        stop_dict = {'id': stop.id,
+                     'name': stop.name,
+                     'location': latlon_json(stop.geo)
+                     }
+        if location:
+            stop_dict['distance'] = stop.distance
+
+        stops.append(stop_dict)
+
+    return {"bus_stops": stops}
+
+@route('/nearest')
+def getnearestsop(db):
+    location = (request.query.decode()['lon'], request.query.decode()['lat'])
+    location = 'POINT(%s %s)' % location
+
+    query = db.query(BusStop.id,
+                     BusStop.name,
+                     BusStop.location.ST_AsGeoJSON().label('geo'),
+                     BusStop.location.ST_Distance_Sphere(location).label('distance')
+                     ).order_by('distance').first()
+
+
+    return {'id': query.id,
+            'name': query.name,
+            'distance': query.distance,
+            'location': latlon_json(query.geo)
+            }
+
+@route('/delete')
+def deleteallstops(db):
+    db.query(BusStop).delete()
+
+@route('/')
+def show_home():
+    return template('main')
+
+@route('/static/<path:path>')
+def callback(path):
+    return static_file(path, root="./static/")
+
+def latlon_json(geoStr):
+    geoJSON = json.loads(geoStr)
+    cords = geoJSON['coordinates']
+    return {'lon': cords[0], 'lat': cords[1]}
+
+if HEROKU:
+    run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='0.0.0.0', port=8080, debug=True, reloader=True)
---
+++ views/main.tpl
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Hey There!</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    <script src="/static/geoPosition.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+
+<p>Your location is: <span id="loc">...</span></p>
+
+<p>Your nearest U1 bus stop is: <span id="nearest">...</span></p>
+
+<script type="text/javascript">
+    var locElmt = document.getElementById("loc");
+    var nearestElmt = document.getElementById("nearest");
+    if(geoPosition.init()){  // Geolocation Initialisation
+        geoPosition.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
+    } else {
+            // You cannot use Geolocation in this device
+        locElmt.innerHTML = "Damn, can't get you location. Sorry :(";
+    }
+
+    // p : geolocation object
+    function success_callback(p){
+        // p.latitude : latitude value
+        // p.longitude : longitude value
+        console.log(p);
+        coords = p.coords;
+        locElmt.innerHTML = coords.latitude + ", " + coords.longitude;
+
+        $.getJSON("/nearest", {
+            lat: coords.latitude,
+            lon: coords.longitude
+        })
+          .done(function(data){
+            nearestElmt.innerHTML = data.name;
+          });
+    }
+
+    function error_callback(p){
+        // p.message : error message
+        locElmt.innerHTML = "Damn, can't get you location. Sorry :(";
+    }
+</script>
+
+</body>
+</html>
+<html>
